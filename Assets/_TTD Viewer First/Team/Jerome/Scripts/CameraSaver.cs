73e3a1e8226ecf491dc437b00e8414d701a46979
JeromeJ <jerome.jamart.42@gmail.com>
2017-12-19 17:01:35 +0100

    Saving given camera positions on click by scenes

---
+++ Assets/_TTD Viewer First/Team/Jerome/Scripts/CameraSaver.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+class CameraSaver : EditorWindow
+{
+    // Use static so it persists when closing the window
+    // (Doesn't persist on Unity restart)
+    static public Dictionary<string, List<Transform>> snapPositions = new Dictionary<string, List<Transform>>();
+    static public Camera camera;
+
+    [MenuItem("Tools/Capture positions")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(CameraSaver));
+    }
+
+    void OnGUI()
+    {
+        camera = EditorGUILayout.ObjectField("Camera", camera, typeof(Camera), true) as Camera;
+
+        if (GUILayout.Button("Save this position") && camera != null)
+        {
+            string currentScene = SceneManager.GetActiveScene().path;
+
+            List<Transform> positions;
+
+            if(!snapPositions.TryGetValue(currentScene, out positions))
+            {
+                positions = new List<Transform>();
+                snapPositions.Add(currentScene, positions);
+            }
+
+            positions.Add(camera.transform);
+        }
+    }
+}
